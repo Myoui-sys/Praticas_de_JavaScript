1438f0ab1a6303c5d2b5ff3ff81150816bd6f3d3
Myoui-sys <meninagamer55@gmail.com>
2026-06-12 20:50:59 -0300

    Refactor logical operations examples to use variables for clarity

--- Praticas/praticasAula05.js
+++ Praticas/praticasAula05.js
@@ -1,12 +1,38 @@
 // 5 é maior que 20 e também é menor que 2
-console.log(5 > 20 && 5 < 2);
+let questao1 = 5 > 20 && 5 < 2;
+console.log(questao1);
 
 // 5 é igual a 5 ou é igual a "5"
-console.log(5 === 5 || 5 === "5");
+let questao2 = 5 === 5 || 5 === "5";
+console.log(questao2);
 
 // Negação (Vinte é maior que cinqueta)
-console.log(!(5 > 20));
+let questao3 = !(5 > 20);
+console.log(questao3);
 
 // Negação (vinte é maior que cinquenta OU cinquenta é maior que sessenta)
+let questao4 = !(20 > 50 || 50 > 60)
+console.log(questao4); 
 
-console.log(!(20 > 50 || 50 > 60)); 
+console.log("-".repeat(50));
+console.log("Atividade")
+
+// 10 é maior que 5 e também é menor que 20
+let questao5 = 10 > 5 && 10 < 20;
+console.log(questao5);
+
+// 15 é menor que 10 ou é igual a 15
+let questao6 = 15 < 10 || 15 == 15;
+console.log(questao6);
+
+// Negação (30 é igual a 40)
+let questao7 = !(30 == 40);
+console.log(questao7);
+
+// Negação (8 é maior que 3 E 12 é menor que 5)
+let questao8 = !(8 > 3 && 12 < 5);
+console.log(questao8);
+
+//25 é maior que 20 OU 10 é maior que 50
+let questao9 = 25 > 20 || 10 > 50;
+console.log(questao9);
