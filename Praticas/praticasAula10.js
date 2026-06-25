68624ff71b258c039a69544886fd90df3fc133e8
Myoui-sys <meninagamer55@gmail.com>
2026-06-25 19:47:47 -0300

    Add game and exercise loop logic with console output

---
+++ Praticas/praticasAula10.js
@@ -0,0 +1,21 @@
+// Jogar enquanto o usuário tiver vida disponível
+
+let vidas = 3;
+
+while (vidas > 0) {
+    console.log("Jogando...");
+    vidas--;
+}
+
+console.log("Game Over!")
+
+// Fazer exercícios enquanto a quantidade de repetições não for atingida
+
+let repeticoes = 1;
+
+while (repeticoes <= 10) {
+    console.log("Fazendo repetição ", repeticoes);
+    repeticoes++;
+}
+
+console.log("Exercício Concluído!");
