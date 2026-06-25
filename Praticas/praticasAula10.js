c375f5865d3bd0040989ba109ca2ac0ec56aa921
Myoui-sys <meninagamer55@gmail.com>
2026-06-25 20:01:04 -0300

    Add series episode watching and login attempt logic with console output

--- Praticas/praticasAula10.js
+++ Praticas/praticasAula10.js
@@ -19,3 +19,25 @@ while (repeticoes <= 10) {
 }
 
 console.log("Exercício Concluído!");
+
+// Assistir episódios de uma série
+
+let episodio = 1;
+let totalEpisodios = 5;
+
+while (episodio <= totalEpisodios) {
+    console.log("Assistindo episódio ", episodio);
+    episodio++;
+}
+
+console.log("Série finalizada!");
+
+// Tentativas de Login 
+let tentativas = 1;
+
+while (tentativas <= 3) {
+    console.log("Tentativa de login nº ", tentativas);
+    tentativas++;
+}
+
+console.log("Número máximo de tentativas atingido.");
