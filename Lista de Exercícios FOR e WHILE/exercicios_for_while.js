811955d37464c8c8025fa87b36fabbda99281854
Myoui-sys <meninagamer55@gmail.com>
2026-06-26 21:11:25 -0300

    Add while loop exercises for number display and sum calculation

---
+++ Lista de Exercícios FOR e WHILE/exercicios_for_while.js
@@ -0,0 +1,35 @@
+// 1 - Exiba os números de 1 a 10 utilizando while
+
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+// 2 - Exiba os números de 10 a 1 utilizando while
+
+let j = 10;
+while (j >= 1) {
+    console.log(j);
+    j--;
+}
+
+// 3 - Exiba apenas os números pares de 2 a 20 utilizando while.
+
+let par = 2;
+while (par <= 20) {
+    console.log(par);
+    par+=2;
+}
+
+// 4 - Calcule e exiba a soma dos números de 1 a 100 utilizando while
+
+let k = 1;
+let soma = 0;
+
+while (k <= 100) {
+    soma += k;
+    k++;
+    console.log(soma)
+}
+
